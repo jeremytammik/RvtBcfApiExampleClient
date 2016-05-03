1912b9586bcb2ec0219f89bc0fc4d8aa98992bd9
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2016-05-03 17:33:43 +0200

    successfully tested

--- RvtBcfApiExampleClient/Properties/AssemblyInfo.cs
+++ RvtBcfApiExampleClient/Properties/AssemblyInfo.cs
@@ -32,5 +32,10 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+//
+// History:
+//
+// 2016-05-03 2017.0.0.0 manually converted from default Visual Studio 2017 wizard-generated class library to Revit add-in
+//
 [assembly: AssemblyVersion("2017.0.0.0")]
 [assembly: AssemblyFileVersion("2017.0.0.0")]
